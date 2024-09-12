6028af5aabda374bf03a72f52a9590e6fed683df
MagomedovMurad20 <magdev09@gmail.com>
2024-09-12 14:23:01 +0300

    прописал роуты и страницы для всех страниц

--- app.py
+++ app.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+@app.route('/clouds')
+def clouds():
+    return render_template('clouds.html')
+
+@app.route('/auto')
+def auto():
+    return render_template('auto.html')
+
+@app.route('/realty')
+def realty():
+    return render_template('realty.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
